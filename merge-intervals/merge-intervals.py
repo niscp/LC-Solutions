b71fddcab647f9099e5ed63f7b3108c9a41c281d
niscp <nishank.cp@gmail.com>
2021-10-29 05:39:16 +0530

    Time: 84 ms (84.92%), Space: 15.9 MB (97.59%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,21 +1,19 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key =lambda x:x[0])
         if len(intervals) < 2:
             return intervals
-        intervals.sort(key = lambda x:x[0])
         result = []
-        start = intervals[0][0]
-        end = intervals[0][1]
+        interval_start = intervals[0][0]
+        interval_end = intervals[0][1]
         for interval in intervals[1:]:
-            # overlapping situation
-            if interval[0] <= end:
-                end = max(end, interval[1])
+            if interval[0] > interval_end:
+                # enw interval
+                result.append([interval_start, interval_end])
+                interval_start = interval[0]
+                interval_end = interval[1]
             else:
-                result.append([start, end])
-                start = interval[0]
-                end = interval[1]
-        result.append([start, end])
-            
-        
+                interval_start = min(interval_start, interval[0])
+                interval_end = max(interval_end, interval[1])
+        result.append([interval_start, interval_end])
         return result
-        
